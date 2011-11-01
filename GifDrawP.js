4b9134598bb9d813f44d8956634803630a7970f3
James Hicks <james@dieneuemodern.com>
2011-11-01 14:27:31 +0000

    Onion Skin and beginning of GIF Export

--- GifDrawP.js
+++ GifDrawP.js
@@ -1,40 +1,50 @@
+
+
+    window.pjsin = Processing.getInstanceById("drawcanvas");
 	var saved = PImage;
 	var currentFrame = 0;
 	var frames=new Array(10);
 	var totalFrames = frames.length;
+    var onionSkin = true;
+    window.totalFramesForEncoder = totalFrames;
 	$("#framenumber").attr("max",totalFrames-1);
+	$("#framenumber").val(currentFrame);
+	
+    PGraphics testimg = createGraphics(500,500,RGB);
     
+    font = loadFont("Courier-12.vlw"); 
+    textFont(font); 
+	
 	void setup() {  
 		size(500,500);  
 		stroke(0);
-		noSmooth();
+		fill(0);
 		strokeWeight(5);
-		
 		for (var i=0; i < frames.length; i++) {
-		  frames[i] = get();
+            frames[i] = createGraphics(500,500,RGB);
+            frames[i].strokeWeight(5);
 		}
-        processingTest();
+        noSmooth();
         
 	};
 	
-	void draw() {};
+	void draw() {
+	    
+	};
 	
 	void mouseDragged() {  
 		line(pmouseX,pmouseY,mouseX,mouseY);
+        frames[currentFrame].beginDraw();
+		frames[currentFrame].line(pmouseX,pmouseY,mouseX,mouseY);
+        frames[currentFrame].endDraw();
 	
 	};
 	
 	void keyPressed() {
-		if (key == "0") { switchFrame(0); };
-		if (key == "1") { switchFrame(1); };
-		if (key == "2") { switchFrame(2); };
-		if (key == "3") { switchFrame(3); };
-		if (key == "4") { switchFrame(4); };
-		if (key == "5") { switchFrame(5); };
-		if (key == "6") { switchFrame(6); };
-		if (key == "7") { switchFrame(7); };
-		if (key == "8") { switchFrame(8); };
-		if (key == "9") { switchFrame(9); };
+	    console.log(key);
+		if (key == 97) {
+            image(testimg,0,0);
+		};
 		
 		if (key == CODED) {
 		    if (keyCode == LEFT)    {prevFrame();}
@@ -44,19 +54,31 @@
 	};
 	
 	void switchFrame(framenum){
-		console.log("Switching to Frame "+framenum);
-		frames[currentFrame] = get();
-		image(frames[framenum],0,0);
-		currentFrame = framenum;
+        gotoFrame(framenum);
         $("#framenumber").val(currentFrame);
 	};
 	
 	void gotoFrame(framenum){
 	    console.log("Going to Frame "+framenum);
-		frames[currentFrame] = get();
+        // frames[currentFrame] = get();
 		image(frames[framenum],0,0);
+		if (onionSkin) {
+            tint(255, 50);
+            image(frames[getOnionFrame(framenum)],0,0);
+    		noTint();		    
+		};
 		currentFrame = framenum;
+        text("Frame "+currentFrame, 20, 20); 
 	};
+	
+	void getOnionFrame(frame){
+	    if (frame != 0) {
+	        return frame - 1;
+	    } else {
+	        return 0;
+	    };
+	}
+	
 
 	void whatsLastFrame(){
 	    if (currentFrame == 0) {
@@ -76,5 +98,12 @@
     
     void nextFrame(){
         switchFrame((currentFrame + 1) % (totalFrames));
-        
     };
+    
+    void setOnionSkin(value){
+        onionSkin = value;
+        gotoFrame(currentFrame);
+        
+        console.log("Onion Skin: "+onionSkin);
+    }
+    
--- GifDrawUI.js
+++ GifDrawUI.js
@@ -1,16 +1,34 @@
 $(document).ready(function() {
-
-
-
-
-    console.log(window.pjsin);
-    $("#framenumber").bind('change', function(event) {
+    $('#framenumber').bind('change', function(event) {
         window.pjsin.gotoFrame($("#framenumber").val());
     });
 	
-    
+    $('#onionSkin').bind('change', function(event) {
+        window.pjsin.setOnionSkin($('#onionSkin').is(":checked"));
+         console.log("frams "+$('#framenumber').attr('max'));
+    });
 
-
+    var encoder = new GIFEncoder();
+    encoder.setRepeat(0);
+    encoder.setDelay(100); 
+    var canvas = document.getElementById('drawcanvas');
+    var context = canvas.getContext('2d');
+   
+    $('#encode').bind('click', function(event) {
+        console.log("ENCODING MOTHERFUCKER");
+        encoder.start();
+        for (var i=0; i < window.totalFramesForEncoder; i++) {
+           window.pjsin.switchFrame(i);
+           console.log("Adding Frame:"+i+" of "+window.totalFramesForEncoder);
+           encoder.addFrame(context);
+        }
+                
+        encoder.finish();
+        // var binary_gif = encoder.stream().getData() 
+        // var data_url = 'data:image/gif;base64,'+encode64(binary_gif);
+        document.getElementById('output').src = 'data:image/gif;base64,'+encode64(encoder.stream().getData())
+        
+    });
     
     
 });
